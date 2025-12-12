117efc7900c50d106fff70edc70eeb84191f045b
Volodimir <sanin.vova200@gmail.com>
2025-12-12 16:35:47 +0200

    fix routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,8 +8,8 @@ const router = Router();
 
 router.post('/register', celebrate(registerSchema), ctrl.register);
 router.post('/login', celebrate(loginSchema), ctrl.login);
-router.post('/logout', celebrate(), ctrl.logout);
-router.get('/current', celebrate(), ctrl.getCurrent);
-router.post('/refresh', celebrate(), ctrl.refresh);
+router.post('/logout', ctrl.logout);
+// router.get('/current', ctrl.getCurrent);
+router.post('/refresh', ctrl.refresh);
 
 export default router;
