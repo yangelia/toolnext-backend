de201c19ea9c705032dbbce23ebb6f8bcf2a26a5
yangelia <yana.sychova@gmail.com>
2025-12-12 20:11:11 +0200

    edit server and env

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,7 +1,9 @@
+import Joi from 'joi';
+import { Segments } from 'celebrate';
+
 export const registerSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).required(),
+    password: Joi.string().min(6).required(),
   }),
 };
-export const loginSchema = {};
