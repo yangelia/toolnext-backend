9a200a738cf5af416133cb2ffdacfeab497693b5
Katerynakulik <katerynakulik4@gmail.com>
2025-12-13 12:14:38 +0200

    Change auths service and middleware

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,29 +1,38 @@
+import crypto from 'crypto';
+import { Session } from '../models/session.js';
 import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
-export const registerService = async () => {};
-export const loginService = async () => {};
-export const logoutService = async () => {};
-export const getCurrentService = async () => {};
-export const refreshService = async () => {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export const createSession = async (userId) => {
+  return Session.create({
+    userId,
+    accessToken: crypto.randomUUID(),
+    refreshToken: crypto.randomUUID(),
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
+};
 
 export const setSessionCookies = (res, session) => {
-  res.cookie('accessToken', session.accessToken, {
+  const options = {
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+  };
+
+  res.cookie('accessToken', session.accessToken, {
+    ...options,
     maxAge: FIFTEEN_MINUTES,
   });
 
   res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    ...options,
     maxAge: ONE_DAY,
   });
 
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+  res.cookie('sessionId', session._id.toString(), {
+    ...options,
     maxAge: ONE_DAY,
   });
 };
