b6291446f831833f6b52ff8fa38c06ccfe76e999
Maryna Tkachenko <marynapanchenko19@gmail.com>
2025-12-13 17:02:09 +0200

    synchronization of models

--- src/models/tool.js
+++ src/models/tool.js
@@ -1,16 +1,62 @@
+// src/models/Tool.js
+
 import { Schema, model } from 'mongoose';
 
 const toolSchema = new Schema(
   {
-    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    name: { type: String, required: true },
-    description: String,
-    pricePerDay: Number,
-    rating: { type: Number, min: 0, max: 5, default: 0 },
-    images: [String],
-    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-    rentalTerms: String,
-    specifications: { type: Schema.Types.Mixed, default: {} },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: false,
+      default: '',
+      trim: true,
+    },
+
+    pricePerDay: {
+      type: Number,
+      required: true,
+    },
+
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+
+    images: {
+      type: [String],
+      default: [],
+    },
+
+    specifications: {
+      type: Map,
+      of: String,
+      default: {},
+    },
+
+    // specifications: { type: Schema.Types.Mixed, default: {} },
+
+    rentalTerms: {
+      type: String,
+      default: '',
+    },
     bookedDates: {
       type: [
         {
@@ -27,7 +73,19 @@ const toolSchema = new Schema(
       },
     ],
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+toolSchema.index(
+  { name: 'text', description: 'text' },
+  {
+    name: 'ToolTextIndex',
+    weights: { name: 5, description: 1 },
+    default_language: 'english',
+  },
 );
 
 export const Tool = model('Tool', toolSchema);
