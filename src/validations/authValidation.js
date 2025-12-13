aed4aa6fb15ef9fa274934e5e3bcefc632a3ab68
LisTRK <listrk54@gmail.com>
2025-12-13 02:35:58 +0200

    added reset pass and email

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,7 +1,26 @@
 import { Joi, Segments } from 'celebrate';
+
+export const loginSchema = {}
+
 export const registerSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
   }),
 };
+
+
+export const requestResetEmailSchema = {
+    [Segments.BODY]: Joi.object({
+        email: Joi.string().email().required(),
+    })
+}
+
+
+
+export const resetPasswordSchema = {
+    [Segments.BODY]: Joi.object({
+        password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
+    })
+}
