307746e36838f8785e9d5c4cd4398e97a7f9b587
Glib-xx <sevensky.ga@gmail.com>
2025-12-13 21:28:53 +0200

    user - profile

--- src/models/session.js
+++ src/models/session.js
@@ -1,16 +1,1 @@
-import { Schema, model } from 'mongoose';
-
-const sessionSchema = new Schema(
-  {
-    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-
-    accessToken: { type: String, required: true },
-    refreshToken: { type: String, required: true },
-
-    accessTokenValidUntil: { type: Date, required: true },
-    refreshTokenValidUntil: { type: Date, required: true },
-  },
-  { timestamps: true },
-);
-
-export const Session = model('Session', sessionSchema);
+с;
