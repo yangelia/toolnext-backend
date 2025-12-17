2e835e0e3df318c65a16af1321889c65d13035e3
Maryna Tkachenko <marynapanchenko19@gmail.com>
2025-12-17 22:51:50 +0200

    add get, patch and delete routes

--- src/routes/toolsRoutes.js
+++ src/routes/toolsRoutes.js
@@ -1,15 +1,36 @@
 import { Router } from 'express';
-import { createTool } from '../controllers/toolsController.js';
-import { upload } from '../middleware/upload.js';
-import { authenticate } from '../middleware/authenticate.js';
+import * as ctrl from '../controllers/toolsController.js';
+import { upload } from '../middleware/upload.js'; // multer
+import { authenticate } from '../middleware/authenticate.js'; // інструменти створюють авторизовані
+
+import { celebrate } from 'celebrate';
+import {
+  toolIdParamsSchema,
+  updateToolSchema,
+  getAllToolsSchema,
+  toolIdSchema,
+} from '../validations/toolValidation.js';
 
 const router = Router();
 
-router.post(
-  '/',
+// PUBLIC ROUTES
+router.get('/', celebrate(getAllToolsSchema), ctrl.getTools);
+router.get('/:toolId', celebrate(toolIdSchema), ctrl.getToolById);
+
+// PROTECTED ROUTES
+router.post('/', authenticate, upload.single('image'), ctrl.createTool);
+router.patch(
+  '/:id',
   authenticate,
-  upload.array('images', 5),
-  createTool
+  upload.single('image'),
+  celebrate(updateToolSchema),
+  ctrl.updateTool,
+);
+router.delete(
+  '/:id',
+  authenticate,
+  celebrate(toolIdParamsSchema),
+  ctrl.deleteTool,
 );
 
 export default router;
