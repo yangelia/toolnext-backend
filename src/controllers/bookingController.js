189f501330bf1bbbbe326ac022dac121bc61f77e
Volodimir <sanin.vova200@gmail.com>
2025-12-18 00:10:03 +0200

    miss close };

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,44 +1,41 @@
-
-import Booking from "../models/booking.js";
-import { createBooking } from "../services/booking.js";
+import Booking from '../models/booking.js';
+import { createBooking } from '../services/booking.js';
 
 // Отримання бронювань користувача
 export const getMyBookings = async (req, res, next) => {
-    try {
-        const userId = req.user.id;
-
-        const bookings = await Booking
-            .find({ userId })
-            .populate("toolId")
-            .sort({ createdAt: -1 });
-
-        return res.json({
-            message: "My bookings retrieved successfully",
-            bookings,
-        });
-    } catch (error) {
-        next(error);
-    }
-
-export const createBookingController = async (req, res, next) => {
   try {
-    const userId = req.user._id
-    const { toolId } = req.params;
+    const userId = req.user.id;
 
-    const booking = await createBooking(userId, toolId, req.body);
+    const bookings = await Booking.find({ userId })
+      .populate('toolId')
+      .sort({ createdAt: -1 });
 
-     if (booking.status === 409) {
-      return res.status(booking.status).json(booking);
-    }
-
-    res.status(201).json({
-      status: 201,
-      message: "Booking created successfully",
-      data: booking,
+    return res.json({
+      message: 'My bookings retrieved successfully',
+      bookings,
     });
   } catch (error) {
     next(error);
   }
 };
 
+export const createBookingController = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+    const { toolId } = req.params;
 
+    const booking = await createBooking(userId, toolId, req.body);
+
+    if (booking.status === 409) {
+      return res.status(booking.status).json(booking);
+    }
+
+    res.status(201).json({
+      status: 201,
+      message: 'Booking created successfully',
+      data: booking,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
