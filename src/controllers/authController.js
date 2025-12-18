de8d6cd21481393dd0685b7257e8bea89068bdee
Volodimir <sanin.vova200@gmail.com>
2025-12-18 20:40:45 +0200

    change login response

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -72,8 +72,12 @@ export const login = async (req, res, next) => {
   setSessionCookies(res, session);
 
   res.status(200).json({
-    id: user._id,
+    _id: user._id,
     email: user.email,
+    username: user.username,
+    avatar: user.avatar,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
   });
 };
 
